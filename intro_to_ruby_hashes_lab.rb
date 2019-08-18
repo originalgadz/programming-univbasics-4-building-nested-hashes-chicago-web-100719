02afc828be8fc60103eebb7f860613e47bb835d4
Glenn A Doherty <gadz@earthlink.net>
2019-08-18 19:55:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	{:railroads => {:pieces => 4, :rent_in_dollars => {:first => {}, :second => {}, :third => {}, :fourth => {}}, :names => {}}}
+	{:railroads => {:pieces => 4, :rent_in_dollars => {:first => {}, :second => {}, :third => {}, :fourth => {}}, :names => {:first => {}, :second => {}, :third => {}, :fourth => {}}}}
 end
 
 def monopoly_with_fourth_tier
